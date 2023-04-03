158760745959b2c8ad176c9f445a21623f7d4211
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2023-04-03 15:16:05 +0900

    改行が読みにくかったので修正

--- src/main.cpp
+++ src/main.cpp
@@ -109,6 +109,7 @@ int main()
 		| btIDebugDraw::DBG_DrawContactPoints
 		| btIDebugDraw::DBG_DrawConstraints);
 
+
 	//
 	// Bullet
 	//
@@ -116,24 +117,18 @@ int main()
 	///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
 	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
 
-
 	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
 	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
-
 
 	///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
 	btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase();
 
-
 	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
 	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
 
-
 	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
 
-
 	dynamicsWorld->setGravity(btVector3(0, -1, 0));
-
 
 	///-----initialization_end-----
 
@@ -142,36 +137,21 @@ int main()
 	//make sure to re-use collision shapes among rigid bodies whenever possible!
 	btAlignedObjectArray<btCollisionShape*> collisionShapes;
 
-
 	///create a few basic rigid bodies
-
 
 	//the ground is a cube of side 100 at position y = -56.
 	//the sphere will hit it at y = -6, with center at -5
 	{
 		btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
-
-
 		collisionShapes.push_back(groundShape);
-
 
 		btTransform groundTransform;
 		groundTransform.setIdentity();
 		groundTransform.setOrigin(btVector3(0, -56, 0));
 
-
 		btScalar mass(0.);
-
-
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
-
-
 		btVector3 localInertia(0, 0, 0);
-		if (isDynamic)
-			groundShape->calculateLocalInertia(mass, localInertia);
-
-
+	
 		//using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
 		btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
 		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
@@ -186,12 +166,10 @@ int main()
 	{
 		//create a dynamic rigidbody
 
-
 		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
 		// btCollisionShape* colShape = new btSphereShape(btScalar(1.));
 		btCollisionShape* colShape = new btCapsuleShape(btScalar(1.), btScalar(3.));
 		collisionShapes.push_back(colShape);
-
 
 		/// Create Dynamic Objects
 		btTransform startTransform;
@@ -199,31 +177,22 @@ int main()
 		startTransform.setRotation(btQuaternion(0.f, 0.f, 0.5f, 1.f));
 
 		btScalar mass(1.f);
-
-
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
-
-
 		btVector3 localInertia(0, 0, 0);
-		if (isDynamic)
-			colShape->calculateLocalInertia(mass, localInertia);
-
+		colShape->calculateLocalInertia(mass, localInertia);
 
 		startTransform.setOrigin(btVector3(2, 10, 0));
-
 
 		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
 		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
 		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
 		btRigidBody* body = new btRigidBody(rbInfo);
 
-
 		dynamicsWorld->addRigidBody(body);
 	}
 
 	// デバック用のインスタンス割当て
 	dynamicsWorld->setDebugDrawer(&debugDraw);
+
 
 	//
 	// Imguiの設定
